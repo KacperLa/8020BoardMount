e455d620d585ff5b58c0e88b3fbeae612d9cf36f
KacperLa <klaskak@gmail.com>
2024-12-15 20:01:59 -0500

    works but has issues

--- 8020BoardMount.groovy
+++ 8020BoardMount.groovy
@@ -1,41 +1,81 @@
 // code here
-ArrayList<CSG> makePCBMount (
+CSG makePCBMount (
     double screwThreadRadius,
     double standoffHeight,
     double standoffRadius,
     double width,
-    double height
+    double height,
+    double baseThickness,
+    double chamfer
     )
 {
-    // Standoff definition
-    
 
- 	CSG standoffBase = new Cylinder(standoffRadius, 2).toCSG();
+	CSG standoffBase = new Cylinder(standoffRadius, baseThickness).toCSG();
 	ArrayList<CSG> standoffs = new ArrayList<CSG>();
-    	standoffs.add(standoffBase.movex(width/2).movey(width/2));
-     standoffs.add(standoffBase.movex(width/2).movey(-width/2));
-     	standoffs.add(standoffBase.movex(-width/2).movey(-width/2));
-     	standoffs.add(standoffBase.movex(-width/2).movey(width/2));
+	standoffs.add(standoffBase.movex(width/2).movey(height/2));
+	standoffs.add(standoffBase.movex(width/2).movey(-height/2));
+	standoffs.add(standoffBase.movex(-width/2).movey(-height/2));
+	standoffs.add(standoffBase.movex(-width/2).movey(height/2));
 
 	CSG basePlate = CSG.hullAll(standoffs);
 
-
 	// Add standoffs to baseplate
 	CSG standoff = new Cylinder(standoffRadius, 10).toCSG();
-     standoff = standoff.difference(new Cylinder(screwThreadRadius, 10).toCSG());
+	standoff = standoff.difference(new Cylinder(screwThreadRadius, 10).toCSG());
 
-     basePlate = basePlate.union(standoff.movex(width/2).movey(width/2));
-     basePlate = basePlate.union(standoff.movex(width/2).movey(-width/2));
-     basePlate = basePlate.union(standoff.movex(-width/2).movey(-width/2));
-     basePlate = basePlate.union(standoff.movex(-width/2).movey(width/2));
-	
-	return [basePlate];
+     basePlate = basePlate.union(standoff.movex(width/2).movey(height/2));
+     basePlate = basePlate.union(standoff.movex(width/2).movey(-height/2));
+     basePlate = basePlate.union(standoff.movex(-width/2).movey(-height/2));
+     basePlate = basePlate.union(standoff.movex(-width/2).movey(height/2));
+
+	CSG standoffChamferBase = new Cylinder(standoffRadius+chamfer, baseThickness+chamfer).toCSG();
+	ArrayList<CSG> standoffChamferBaseList = new ArrayList<CSG>();
+	standoffChamferBaseList.add(standoffChamferBase.movex(width/2).movey(height/2));
+	standoffChamferBaseList.add(standoffChamferBase.movex(width/2).movey(-height/2));
+	standoffChamferBaseList.add(standoffChamferBase.movex(-width/2).movey(-height/2));
+	standoffChamferBaseList.add(standoffChamferBase.movex(-width/2).movey(height/2));
+
+	for (int i = 0; i < standoffChamferBaseList.size(); i++)
+	{
+		basePlate = basePlate.union(basePlate.intersect(standoffChamferBaseList[i]).hull());
+	}
+
+	CSG screwHole = new Cylinder(screwThreadRadius, standoffHeight+baseThickness).toCSG();
+
+     basePlate = basePlate.difference(screwHole.movex(width/2).movey(height/2));
+     basePlate = basePlate.difference(screwHole.movex(width/2).movey(-height/2));
+     basePlate = basePlate.difference(screwHole.movex(-width/2).movey(-height/2));
+     basePlate = basePlate.difference(screwHole.movex(-width/2).movey(height/2));
+
+	// mounting holes
+	CSG mountHole = new Cylinder((5.5)/2, baseThickness).toCSG();
+     basePlate = basePlate.difference(mountHole.movex(width/3));
+     basePlate = basePlate.difference(mountHole.movex(-width/3));
+     
+	return basePlate;
 }
 
-return makePCBMount(
-				screwThreadRadius = 2.8/2.0,
-		     		standoffHeight = 5,
-		     		standoffRadius = 3,
-		     		width  = 30,
-		    		height = 30
-                    );
+CSG makePCBMountFromParams(){
+	screwThreadD   = new LengthParameter("Diameter of the thread of the screw", 2.8, [120.0, 0.5]);
+	standoffHeight = new LengthParameter("Height of standoffs ", 10,[100.0, 0]);
+	standoffD   	= new LengthParameter("Standoff Diameter", 6,[140.0,3.0]);
+	width 	     = new LengthParameter("width between mounting holes", 30,[150.0,10.0]);
+	height 	     = new LengthParameter("height between mounting holes", 30,[150.0,10.0]);
+	baseThickness 	= new LengthParameter("Thickness of baseplate", 3,[150.0,1.0]);
+	chamfer 	     = new LengthParameter("Chanfer betwwen standoffs and baseplate", 3,[150.0, 0.0]);
+
+	CSG mount = makePCBMount(
+				screwThreadRadius = screwThreadD.getMM()/2.0,
+				standoffHeight    = standoffHeight.getMM(),
+				standoffRadius    = standoffD.getMM()/2.0,
+				width             = width.getMM(),
+				height            = height.getMM(),
+				baseThickness     = baseThickness.getMM(),
+				chamfer           = chamfer.getMM()
+				);
+
+	return mount.setRegenerate({ makePCBMountFromParams()})
+}
+
+//CSGatabase.clear()//set up the database to force only the default values in			
+return makePCBMountFromParams();
